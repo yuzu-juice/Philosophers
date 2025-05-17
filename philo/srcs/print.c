7b85320badcfbfa4c11ec52f91d6e9e9787cb0c3
yuzu-juice <yuzujuicesec@gmail.com>
2025-05-17 19:28:21 +0900

    wip

--- philo/srcs/print.c
+++ philo/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 09:30:41 by takitaga          #+#    #+#             */
-/*   Updated: 2025/05/07 00:44:34 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/05/07 01:24:00 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ t_error	print_status(t_waiter *w, int philo_id, t_print_status s)
 		printf("%ld %d is sleeping\n", elapsed_time, philo_id);
 	else if (s == IS_THINKING)
 		printf("%ld %d is thinking\n", elapsed_time, philo_id);
-	else if (s == DIED)
+	else if (s == DIED && w->should_stop == false)
 		printf("%ld %d died\n", elapsed_time, philo_id);
 	pthread_mutex_unlock(w->print_mutex);
 	return (create_success());
