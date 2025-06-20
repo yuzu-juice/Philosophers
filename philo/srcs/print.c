13791d0aafc5ec478b2a354073a8303709e40f68
yuzu-juice <yuzujuicesec@gmail.com>
2025-06-20 18:58:56 +0900

    update print

--- philo/srcs/print.c
+++ philo/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: takitaga <takitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 09:30:41 by takitaga          #+#    #+#             */
-/*   Updated: 2025/06/20 07:06:39 by takitaga         ###   ########.fr       */
+/*   Updated: 2025/06/20 18:58:41 by takitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 t_error	print_status(t_table *t, int philo_id, t_print_status s)
 {
 	long	elapsed_time;
-	bool	stop;
 
 	elapsed_time = elapsed_time_as_ms(t->start_time);
-	stop = should_stop(t);
-	if (stop)
-		return (create_success());
 	pthread_mutex_lock(&t->print_mutex);
+	if (should_stop(t) && s != DIED)
+	{
+		pthread_mutex_unlock(&t->print_mutex);
+		return (create_success());
+	}
 	if (s == TAKEN_FORK)
 		printf("%ld %d has taken a fork\n", elapsed_time, philo_id);
-	else if (s == TAKEN_FORKS)
-		printf("%ld %d has taken a fork\n%ld %d has taken a fork\n",
-			elapsed_time, philo_id, elapsed_time, philo_id);
 	else if (s == IS_EATING)
 		printf("%ld %d is eating\n", elapsed_time, philo_id);
 	else if (s == IS_SLEEPING)
